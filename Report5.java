7faa848274cc4ab66945b95291cfea5ed2f54da9
Takumu YAMZATO <e215739@ie.u-ryukyu.ac.jp>
2021-12-18 18:14:52 +0900

    3rd commit

--- Report5.java
+++ Report5.java
@@ -1,11 +1,7 @@
 public class Report5 {
-    public static void main(String[] args){
-        try {
-            String str = null;
-            System.out.println(str.length());   
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerExceptionが発生しました。");
-            System.out.println("エラー:" + e.getMessage());
-        }
+    public static void main(String[] args){    
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+        System.out.println(value);   
     }
 }
